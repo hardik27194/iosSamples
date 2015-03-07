133c39e117904ac00f315c6b67a6a8ae18561a2b
takeshi <takeshi@SHIMADATAKESHI-no-MacBook-Pro.local>
2015-03-07 20:00:04 +0900

    aspects を追加

---
+++ objcSample/SampleAspects/SampleBlocks/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  SampleBlocks
+//
+//  Created by 島田武史 on 2014/11/11.
+//  Copyright (c) 2014年 jp.co.sakura.charorin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.view.backgroundColor = [UIColor colorWithRed:0.6 green:1 blue:1 alpha:0.2];
+
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

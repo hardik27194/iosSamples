6eca16d784608801c0e51d62473d331b22e83fde
島田 武史 <shimada_takeshi_gn@cyberagent.co.jp>
2015-06-15 19:28:05 +0900

    fix

--- autoLayoutSmpl/sampleProj/sampleProj/AMOLayoutConstraint.m
+++ autoLayoutSmpl/sampleProj/sampleProj/AMOLayoutConstraint.m
@@ -221,4 +221,235 @@
 }
 
 
+#pragma mark -- snipet 
+//
+//-(void)temp1 {
+//    
+//    
+//    NSArray* constraints =
+//    @[
+//      
+//      [NSLayoutConstraint constraintWithItem:self.childView
+//                                   attribute:NSLayoutAttributeLeft
+//                                   relatedBy:NSLayoutRelationEqual
+//                                      toItem:self.view
+//                                   attribute:NSLayoutAttributeLeft
+//                                  multiplier:1.f constant:50.0f],
+//      
+//      [NSLayoutConstraint constraintWithItem:self.childView
+//                                   attribute:NSLayoutAttributeRight
+//                                   relatedBy:NSLayoutRelationEqual
+//                                      toItem:self.view
+//                                   attribute:NSLayoutAttributeRight
+//                                  multiplier:1.f constant:-50.0f],
+//      
+//      [NSLayoutConstraint constraintWithItem:self.childView
+//                                   attribute:NSLayoutAttributeTop
+//                                   relatedBy:NSLayoutRelationEqual
+//                                      toItem:self.view
+//                                   attribute:NSLayoutAttributeTop
+//                                  multiplier:1.f constant:150.0f],
+//      
+//      [NSLayoutConstraint constraintWithItem:self.childView
+//                                   attribute:NSLayoutAttributeBottom
+//                                   relatedBy:NSLayoutRelationEqual
+//                                      toItem:self.view
+//                                   attribute:NSLayoutAttributeBottom
+//                                  multiplier:1.f constant:-150.0f]
+//      ];
+//    
+//    
+//    NSArray* constraints2 =
+//    @[
+//      [AMOLayoutConstraint constraintWithItem:self.childView
+//                                    attribute:NSLayoutAttributeBottom
+//                                    relatedBy:NSLayoutRelationEqual
+//                                       toItem:self.view
+//                                    attribute:NSLayoutAttributeBottom
+//                                   multiplier:1.f constant:-250.0f]
+//      ];
+//    
+//    for (NSLayoutConstraint* constraint in constraints){
+//        constraint.priority = UILayoutPriorityDefaultHigh;
+//    }
+//    
+//    for (NSLayoutConstraint* constraint in constraints2){
+//        constraint.priority = UILayoutPriorityDefaultLow;
+//    }
+//    
+//    NSLayoutConstraint* constraint3 = [[NSLayoutConstraint alloc] init];
+//    constraint3.priority = UILayoutPriorityFittingSizeLevel;
+//    
+//    
+//    
+//    [self.view addConstraints:constraints2];
+//    //    [self.view addConstraints:constraints];
+//    
+//    for (NSLayoutConstraint* constraint in self.view.constraints){
+//        NSLog(@" priority %f " , constraint.priority );
+//        if ([constraint isKindOfClass:[AMOLayoutConstraint class]]) {
+//            NSLog(@"hjkl;:");
+//            
+//        }
+//    }
+//    
+//    
+//    UIButton* button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+//    CGRect buttonRect = CGRectMake(100, 100, 100, 30);
+//    button.frame = buttonRect;
+//    [button setTitle:@"but" forState:UIControlStateNormal];
+//    [button addTarget:self action:@selector(hoge) forControlEvents:UIControlEventTouchDown];
+//    [self.view addSubview:button];
+//    
+//    
+//    NSInteger cnt = [self.view.constraints count];
+//    NSLog(@" cnt %ld", cnt );
+//    
+//    [self showConstraintDataWithConstrains:self.view.constraints];
+//    
+//    
+//    NSLog(@"stop ");
+//    
+//    
+//    
+//}
+//
+//-(void)hoge {
+//    
+//    NSInteger cnt = [self.view.constraints count];
+//    NSLog(@" cnt %ld", cnt );
+//    
+//    
+//    NSLog(@"hoge");
+//    
+//    /*
+//     for (NSLayoutConstraint* constraint in self.constraints) {
+//     [self.view removeConstraint:constraint];
+//     NSInteger cnt = [self.view.constraints count];
+//     NSLog(@" cnt %ld", cnt );
+//     
+//     }
+//     */
+//    
+//    
+//    cnt = [self.view.constraints count];
+//    
+//    //    [self.view removeConstraints:self.view.constraints];
+//    
+//    
+//    
+//    for (NSLayoutConstraint* constraint in self.view.constraints){
+//        NSLog(@" priority %f " , constraint.priority );
+//        if ([constraint isKindOfClass:[AMOLayoutConstraint class]]) {
+//            NSLog(@"hjkl;:");
+//        }
+//    }
+//    
+//    
+//    [self performSelector:@selector(hoge2) withObject:nil afterDelay:1.0f];
+//    
+//    [self showConstraintDataWithConstrains:self.view.constraints];
+//    
+//    
+//}
+//
+//-(void)hoge2 {
+//    
+//    
+//    for (NSLayoutConstraint* constraint in self.view.constraints){
+//        NSLog(@" priority %f " , constraint.priority );
+//        if ([constraint isKindOfClass:[AMOLayoutConstraint class]]) {
+//            [self.view removeConstraint:constraint];
+//        }
+//    }
+//    
+//    
+//    
+//    NSLog(@"before constraints count %ld ", [self.view.constraints count] );
+//    //
+//    //
+//    //    self.constraints
+//    //    = @[
+//    //        [NSLayoutConstraint constraintWithItem:self.childView
+//    //                                     attribute:NSLayoutAttributeLeft
+//    //                                     relatedBy:NSLayoutRelationEqual
+//    //                                        toItem:self.view
+//    //                                     attribute:NSLayoutAttributeLeft
+//    //                                    multiplier:1.f constant:50.0f],
+//    //
+//    //        [NSLayoutConstraint constraintWithItem:self.childView
+//    //                                     attribute:NSLayoutAttributeRight
+//    //                                     relatedBy:NSLayoutRelationEqual
+//    //                                        toItem:self.view
+//    //                                     attribute:NSLayoutAttributeRight
+//    //                                    multiplier:1.f constant:-50.0f],
+//    //
+//    //        [NSLayoutConstraint constraintWithItem:self.childView
+//    //                                     attribute:NSLayoutAttributeTop
+//    //                                     relatedBy:NSLayoutRelationEqual
+//    //                                        toItem:self.view
+//    //                                     attribute:NSLayoutAttributeTop
+//    //                                    multiplier:1.f constant:150.0f],
+//    //
+//    //        [NSLayoutConstraint constraintWithItem:self.childView
+//    //                                     attribute:NSLayoutAttributeBottom
+//    //                                     relatedBy:NSLayoutRelationEqual
+//    //                                        toItem:self.view
+//    //                                     attribute:NSLayoutAttributeBottom
+//    //                                    multiplier:1.f constant:-250.0f]
+//    //        ];
+//    //
+//    
+//    NSArray* constraints2 =
+//    @[
+//      [AMOLayoutConstraint constraintWithItem:self.childView
+//                                    attribute:NSLayoutAttributeBottom
+//                                    relatedBy:NSLayoutRelationEqual
+//                                       toItem:self.view
+//                                    attribute:NSLayoutAttributeBottom
+//                                   multiplier:1.f constant:-250.0f]
+//      ];
+//    
+//    
+//    //    [self.view addConstraints:self.constraints];
+//    [self.view addConstraints:constraints2];
+//    
+//    
+//    
+//    NSLog(@"after constraints count %ld ", [self.view.constraints count] );
+//    
+//    [self showConstraintDataWithConstrains:self.view.constraints];
+//    
+//    
+//     for (NSLayoutConstraint* constraint in self.view.constraints){
+//     if (constraint.priority == UILayoutPriorityDefaultLow) {
+//     [self.view removeConstraint:constraint];
+//     break;
+//     }
+//     }
+//    
+//    
+//    
+//    NSLog(@"-------------------");
+//    
+//    [self showConstraintDataWithConstrains:self.view.constraints];
+//    
+//    
+//}
+//
+//-(void)showConstraintDataWithConstrains:(NSArray*)constraints{
+//    
+//    for (NSLayoutConstraint* constraint in constraints){
+//        NSLog(@"-------------------");
+//        NSLog(@" pointer %@ ",constraint);
+//        NSLog(@" pointer %p ",constraint);
+//        NSLog(@"   priority %f " , constraint.priority);
+//        
+//    }
+//    
+//}
+//
+//
+
+
 @end

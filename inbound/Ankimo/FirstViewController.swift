9a337d08326fa87680a02c0b4dbd867ed77821b4
takeshi <takeshi@SHIMADATAKESHI-no-MacBook-Pro.local>
2015-07-15 20:56:11 +0900

    non auto layout but code

--- inbound/Ankimo/FirstViewController.swift
+++ inbound/Ankimo/FirstViewController.swift
@@ -33,69 +33,19 @@ class FirstViewController: UIViewController {
         
         
         // itemView
-        let padding = UIEdgeInsetsMake(20, 20, 0, 20)
-        
-        let itemView3 = UINib(nibName: "ItemView3", bundle: nil).instantiateWithOwner(self, options: nil).first as! UIView
-        self.scrollView.addSubview(itemView3)
-
-        let itemView3a = UINib(nibName: "ItemView3", bundle: nil).instantiateWithOwner(self, options: nil).first as! UIView
-        self.scrollView.addSubview(itemView3a)
-
-        let itemView3b = UINib(nibName: "ItemView3", bundle: nil).instantiateWithOwner(self, options: nil).first as! UIView
-        self.scrollView.addSubview(itemView3b)
-
-        let itemView3c = UINib(nibName: "ItemView3", bundle: nil).instantiateWithOwner(self, options: nil).first as! UIView
-        self.scrollView.addSubview(itemView3c)
+        for (var idx = 0; idx <= 8; idx++) {
+            let itemView3 = ItemView3(frame: CGRectZero)
+            itemView3.tag = tagForItemView3(idx)
+            itemView3.frame = frameForItemView(startY: 10.0, idx: idx)
+        }
 
         
-        let y1:CGFloat! = naviCon?.navigationBar.frame.size.height
-        let y2:CGFloat! = naviCon?.navigationBar.frame.origin.y
-        let y = y1 + y2 + 15.0
         
-        itemView3.mas_makeConstraints{make in
-            make.left.equalTo()(self.scrollView.mas_left).with().offset()(15)
-            make.top.equalTo()(self.scrollView.mas_top).with().offset()(y)
-            make.height.equalTo()(100)
-            return ()
-        }
-
-        itemView3a.mas_makeConstraints{make in
-            make.left.equalTo()(itemView3.mas_right).with().offset()(10)
-            make.top.equalTo()(itemView3.mas_top).with().offset()(00)
-            make.width.equalTo()(itemView3.mas_width)
-            make.height.equalTo()(itemView3.mas_height)
-            return ()
-        }
-
-        itemView3b.mas_makeConstraints{make in
-            make.left.equalTo()(itemView3a.mas_right).with().offset()(10)
-            make.top.equalTo()(itemView3.mas_top).with().offset()(00)
-            make.width.equalTo()(itemView3.mas_width)
-            make.height.equalTo()(itemView3.mas_height)
-            return ()
-        }
-
-        itemView3c.mas_makeConstraints{make in
-            make.left.equalTo()(itemView3b.mas_right).with().offset()(10)
-            make.top.equalTo()(itemView3.mas_top).with().offset()(00)
-            make.right.equalTo()(self.view.mas_right).with().offset()(-10)
-            make.width.equalTo()(itemView3.mas_width)
-            make.height.equalTo()(itemView3.mas_height)
-            return ()
-        }
-
         
         // AD特集
         let adFeaturesLabel = UILabel()
         adFeaturesLabel.text = "AD特集"
         self.scrollView.addSubview(adFeaturesLabel)
-        adFeaturesLabel.mas_makeConstraints{make in
-            make.top.equalTo()(itemView3.mas_bottom).with().offset()(20)
-            make.left.equalTo()(itemView3.mas_left).with().offset()(00)
-            make.width.equalTo()(100)
-            make.height.equalTo()(20)
-            return ()
-        }
 
         // 特集１
         let adFeatureView1 = UIView()
@@ -103,25 +53,11 @@ class FirstViewController: UIViewController {
         adFeatureLabel.text = "特集"
         adFeatureView1.addSubview(adFeatureLabel)
         self.scrollView.addSubview(adFeatureView1)
-        adFeatureView1.mas_makeConstraints{make in
-            make.top.equalTo()(adFeaturesLabel.mas_bottom).with().offset()(20)
-            make.left.equalTo()(itemView3.mas_left).with().offset()(00)
-            make.height.equalTo()(20)
-            return ()
-        }
 
         // 特集2
         let adFeatureView2 = UIView()
         adFeatureView2.addSubview(adFeatureLabel)
         self.scrollView.addSubview(adFeatureView2)
-        adFeatureView2.mas_makeConstraints{make in
-            make.top.equalTo()(adFeatureView1.mas_top)
-            make.left.equalTo()(adFeatureView1.mas_right).with().offset()(20)
-            make.right.equalTo()(self.scrollView.mas_right).with().offset()(-20)
-            make.width.equalTo()(adFeatureView1.mas_width)
-            make.height.equalTo()(20)
-            return ()
-        }
 
         
         NSLog("selfview recu %@", self.view.subviews )  // .recursiveDescription)
@@ -130,6 +66,36 @@ class FirstViewController: UIViewController {
     }
     
 
+    func tagForItemView3(idx: NSInteger) -> NSInteger {
+        return 10000 + idx
+    }
+    
+    func frameForItemView(#startY: CGFloat, idx: NSInteger) -> CGRect {
+        
+        var num = posNum(idx: idx, colNum: 4)
+        
+        
+        
+        var posY = 0 as CGFloat
+        
+        let screenWidth = CGRectGetWidth(UIScreen.mainScreen().bounds)
+        let padding = 10.0 as CGFloat
+        let width = (screenWidth - (padding * 5.0)) / 4.0
+
+        let posX = padding + width * (CGFloat)num.x
+
+        
+        var height = 0 as CGFloat
+        
+        return CGRectMake(posX, posY, width, height)
+        
+    }
+    
+    func posNum (#idx: NSInteger, colNum: NSInteger) -> (x: NSInteger, y: NSInteger) {
+        return (x:idx % colNum, y: idx / colNum)
+    }
+    
+    
 }
 
 
--- inbound/Ankimo/View/ItemView3.swift
+++ inbound/Ankimo/View/ItemView3.swift
@@ -11,12 +11,12 @@ import UIKit
 class ItemView3: UIView {
 
     
-    override func awakeFromNib() {
-        NSLog("awake from nib ")
-    }
-
     required init(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
     
     

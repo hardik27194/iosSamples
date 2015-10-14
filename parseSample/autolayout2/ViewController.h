635cba0f7fe83e96c29bee10c46efb8889984af5
島田 武史 <shimada_takeshi_gn@cyberagent.co.jp>
2015-10-14 14:13:00 +0900

    add parse

---
+++ parseSample/autolayout2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  autolayout2
+//
+//  Created by 島田 武史 on 2015/06/15.
+//  Copyright (c) 2015年 島田 武史. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ parseSample/autolayout2/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  autolayout2
+//
+//  Created by 島田 武史 on 2015/06/15.
+//  Copyright (c) 2015年 島田 武史. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+@property (nonatomic, weak) IBOutlet UIView* baseView;
+@property (nonatomic, weak) IBOutlet UIView* childView;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+
+
+
+}
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

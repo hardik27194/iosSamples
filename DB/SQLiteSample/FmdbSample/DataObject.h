4ef722124eb549d0478830bf86c660e2ecbff37e
島田 武史 <shimada_takeshi_gn@cyberagent.co.jp>
2015-11-05 11:45:00 +0900

    once complete sqlite

---
+++ DB/SQLiteSample/FmdbSample/DataObject.h
@@ -0,0 +1,16 @@
+//
+//  tmpObject.h
+//  SqliteSample
+//
+//  Created by 島田 武史 on 2015/11/05.
+//  Copyright © 2015年 AMoAd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataObject : NSObject
+
+-(void)prepareDb;
+-(void)selectDB;
+
+@end
--- DB/SQLiteSample/FmdbSample/ViewController.m
+++ DB/SQLiteSample/FmdbSample/ViewController.m
@@ -7,23 +7,23 @@
 //
 
 #import "ViewController.h"
-
-
+#import "DataObject.h"
 
 @interface ViewController ()
-
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+
 }
 
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
+-(void)viewDidAppear:(BOOL)animated {
+  DataObject *dataObject = [[DataObject alloc] init];
+  [dataObject selectDB];
+
 }
+
 
 @end
